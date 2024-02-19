3779ff9ab08c66e474fe8d4a7bf2c0a6716e9fb0
Konstantin <neversaydievol.6@gmail.com>
2024-02-19 23:35:17 +0700

    add pages

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,24 @@
 <script>
-    import {routes} from './routes'
+
+    import { routes } from "./routes";
+
 </script>
+<svelte:head>
+    <title>Блог</title>
+</svelte:head>
+<div class="container">
+    <h1>Мои статьи</h1>
+    <div class="blogposts">
+        {#each routes as page}
+            <div class="post">
+                <h2>{page.title}</h2>
+                <p>{page.body}</p>
+                <p class="readmore">
+                    <a class="link" href={`/blog/posts/${page.id}`}>
+                        Читать далее
+                    </a>
+                </p>
+            </div>
+        {/each}
+    </div>
+</div>
