ebc9f45ce40bd955550be451234d37522fa448c5
Konstantin <neversaydievol.6@gmail.com>
2024-02-19 21:45:57 +0700

    add main page and nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,31 @@
 <svelte:head>
 <title> Имя Фамилия </title>
 </svelte:head>
+
 <div class='container'>
-<div class='info'>
-<div class='animate-background'>
-<div class='container info-container'>
-<div class='info-text'>
-    <h1>
-    <p class='animate-text-1'>Lorem ipsum dolor sit,</p>
-    <p class='text-2'>Lorem ipsum dolor sit amet.</p>
-    </h1>
+    <div class='info'>
+        <div class='animate-background'>
+            <div class='container info-container'>
+                <div class='info-text'>
+                    <h1>
+                    <p class='animate-text-1'>Lorem ipsum dolor sit,</p>
+                    <p class='text-2'>Lorem ipsum dolor sit amet.</p>
+                    </h1>
+                </div>
+                    <h2 class='subtitle'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto aut,
+                    cupiditate dolores
+                    ducimus expedita labore nemo odio quibusdam quidem sapiente soluta.
+                    </h2>
+                <div style='color:black' class='scroll-button animate-scroll' >
+                <a tabindex='0' href='/about'>Click Here</a>
+                <div class='arrow'>→</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-    <h2 class='subtitle'>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto aut,
-    cupiditate dolores
-    ducimus expedita labore nemo odio quibusdam quidem sapiente soluta.
-    </h2>
-<div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
-<a href='/about'>Click Here</a>
-<div class='arrow'>→</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+
 <div class='background-div'></div>
 
 <style>
