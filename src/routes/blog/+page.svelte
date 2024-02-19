45505ab1014ae8b3f9252d346cacb8fddf3151fc
Konstantin <neversaydievol.6@gmail.com>
2024-02-19 23:03:59 +0700

    add pages

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,19 +1,3 @@
-<svelte:head>
-    <title>Блог</title>
-</svelte:head>
-<div class="container">
-    <h1>Мои статьи</h1>
-    <div class="blogposts">
-        {#each routes as page}
-        <div class="post">
-            <h2>{page.title}</h2>
-            <p>{page.body}</p>
-            <p class="readmore">
-            <a class="link" href={`/blog/posts/${page.id}`}>
-                Читать далее
-            </a>
-            </p>
-        </div>
-        {/each}
-    </div>
-</div>
+<script>
+    import {routes} from './routes'
+</script>
--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -1,9 +1,9 @@
 export const routes = [
-    
     {
-    id: 1,
-    title: 'My First Blog Post',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',  
-    },  
+        id: 1,
 
-    ];    
+        title: 'My First Blog Post',
+
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
+    }
+];
